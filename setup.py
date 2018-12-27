99289e100d7fca1beaa55b22748900a95422e843
fomuon <fomuon@gmail.com>
2018-12-27 14:49:21 +0900

    cli 동작 오류 수정 version 옵션 추가

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+from db2pojo import __version__
+
 setup(
 	name = 'db2pojo',
-	version = '0.1.0',
+	version = __version__,
 	install_requires = ['pymysql'],
 	packages = ['db2pojo'],
 	python_requires  = '>=3',
-	entry_points = { "console_scripts": ["db2pojo = cli:entry_point"] },
+	entry_points = { "console_scripts": ["db2pojo = db2pojo.cli:main"] },
 	author = 'Lee,Yongkyu',
 	author_email = 'fomuon@gmail.com',
 	url = 'https://github.com/fomuon/db2pojo',
