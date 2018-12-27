4ded2a22d475ff1ea093e77dce8ef7d2214e0ace
fomuon <fomuon@gmail.com>
2018-12-27 14:49:21 +0900

    cli 동작 오류 수정 version 옵션 추가

--- db2pojo/cli.py
+++ db2pojo/cli.py
@@ -4,6 +4,7 @@ import argparse
 import codecs
 import os
 
+from db2pojo import __version__
 from db2pojo.mysql import get_tables_from_mysql
 from db2pojo.pojo import convert_pojo
 
@@ -22,7 +23,13 @@ def main():
 	parser.add_argument('--ddl', type=str, metavar='OUTPUT_DIR', help='ddl for create table generation option')
 	parser.add_argument('--dry', action="store_true", help='it will not create files will only print to console.')
 
+	parser.add_argument('--version', default=False, action='store_true', help='Print the current version')
+
 	args = parser.parse_args()
+
+	if args.version:
+		print('excel-export version {}'.format(__version__))
+		exit(0)
 
 	tables = get_tables_from_mysql(args.mysql_host, args.mysql_port, args.db_name, args.mysql_user, args.mysql_password, args.mysql_charset)
 
